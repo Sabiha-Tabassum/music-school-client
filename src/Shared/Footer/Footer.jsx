feb8fc92476b6a075c227595402706a34deec4f4
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-06-07 01:58:05 +0600

    fourth commit

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,30 +1,45 @@
 import React from 'react';
+import logo from '../../images/images.jpg';
 
 const Footer = () => {
     return (
-        <footer className=" p-10 bg-neutral text-neutral-content">
-            <div >
-                <div className='md:flex justify-center gap-4 mx-auto'>
-
-                    <a className="link link-hover">About</a>
-                    <a className="link link-hover">Services</a>
-                    <a className="link link-hover">FAQ</a>
-                    <a className="link link-hover">Legal</a>
-                    <a className="link link-hover">Contact</a>
-
-                </div>
-                <div className='md:ml-96 md:pl-40 mt-4'>
-                    <h4 className=''>Stay in touch</h4>
-                    {/* todo icon */}
-                </div>
-                <div className='md:ml-96 mt-16'>
-                    <p>Copyright © 2023 - All right reserved by </p>
-                </div>
-
-
+        <footer className="footer p-10 bg-base-200 text-base-content">
+            <div>
+                <img style={{height: '80px'}} src={logo} alt="" />
+                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
             </div>
+            <div>
+                <span className="footer-title">Services</span>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </div>
+
+            
         </footer>
     );
 };
 
 export default Footer;
+
+{/* <div className='md:ml-96 md:pl-40 mt-4'>
+<h4 className=''>Stay in touch</h4>
+{/* todo icon */}
+// </div>
+// <div className='md:ml-96 mt-16'>
+// <p>Copyright © 2023 - All right reserved by </p>
+// </div> */}
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,79 @@
 import React from 'react';
+import {  NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const navOptions =
+        <>
+            <div className='mx-4'>
+                <NavLink
+                    to="/"
+
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-sky-500 font-semibold" : ""
+                    }
+                >
+                    Home
+                </NavLink>
+            </div>
+
+            <div className='mx-4'>
+                <NavLink
+                    to="/"
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-sky-500 font-semibold" : ""
+                    }
+                >
+                    Instructors
+                </NavLink>
+            </div>
+
+            
+               <NavLink
+                    to="/"
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-sky-500 font-semibold" : ""
+                    }
+                >
+                    Classes
+                </NavLink>
+            
+
+            <div className='mx-4'>
+                <NavLink
+                    to="/"
+                    className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "text-sky-500 font-semibold" : ""
+                    }
+                >
+                    Dashboard
+                </NavLink>
+            </div>
+
+        </>
     return (
         <div>
-            
+            <div className="navbar  ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {navOptions}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-xl"></a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navOptions}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Login</a>
+                </div>
+            </div>
         </div>
     );
 };
