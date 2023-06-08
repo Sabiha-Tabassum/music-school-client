220676bfbdc9ec1ed2176c30f58268fb57838842
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-06-08 15:37:47 +0600

    seventeen commit

--- src/DashboardPages/ManageUser/ManageUser.jsx
+++ src/DashboardPages/ManageUser/ManageUser.jsx
@@ -1,18 +1,99 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { FaUserShield } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const ManageUser = () => {
     const { data: user = [], refetch } = useQuery(['users'], async () => {
         const res = await fetch('http://localhost:5000/user')
         return res.json();
     })
+
+    const handleMakeAdmin = user => {
+        fetch(`http://localhost:5000/user/admin/${user._id}`,{
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is admin now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
+
+    const handleMakeInstructor = user => {
+        fetch(`http://localhost:5000/user/instructor/${user._id}`,{
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is Instructor now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
+
     return (
         <div>
-             <Helmet>
+            <Helmet>
                 <title>Manage User</title>
             </Helmet>
             <h3 className='text-3xl font-semibold'>Total Users:  {user.length}</h3>
+
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            user.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.role === 'Admin' ? 'Admin' :
+                                    <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost btn-lg"><FaUserShield></FaUserShield></button>}
+                                </td>
+
+                                <td>{user.role === 'Instructor' ? 'Instructor' :
+                                    <button onClick={() => handleMakeInstructor(user)} className="btn btn-ghost btn-lg"><FaUserShield></FaUserShield></button>}
+                                </td>
+
+                               
+                            </tr>
+
+
+                            )
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
