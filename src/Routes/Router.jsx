648743343d1a42e0fe8310122f642873755e22d3
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-06-08 12:15:27 +0600

    thirteen commit

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,35 +1,41 @@
 import {
-    createBrowserRouter,
-    
-  } from "react-router-dom";
+  createBrowserRouter,
+
+} from "react-router-dom";
 
 
 import SignUp from "../SignUp/SignUp";
 import Login from "../Login/Login";
 import Home from "../Pages/Home/Home/Home";
 import Main from "../Layout/Main";
+import Dashboard from "../Pages/Dashboard/Dashboard";
 
 
 
- export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        },
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
 
-        {
-            path: 'signup',
-            element: <SignUp></SignUp>
-        },
+      {
+        path: 'signup',
+        element: <SignUp></SignUp>
+      },
 
-        {
-            path: 'login',
-            element: <Login></Login>
-        }
-      ]
-    },
-  ]);
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+
+      {
+        path: 'dashboard',
+        element: <Dashboard></Dashboard>
+      }
+    ]
+  },
+]);
