942e4f59714b44c32ae972ae140d44d0eb07106a
Sabiha-Tabassum <tsabiha050@gmail.com>
2023-06-08 12:52:28 +0600

    fifteen commit

--- src/Components/SocialLogin/SocialLogin.jsx
+++ src/Components/SocialLogin/SocialLogin.jsx
@@ -15,11 +15,28 @@ const SocialLogin = () => {
     const handleGoogleSignIn = () => {
         googleSignIn()
         .then(result => {
-            const loggedUser = result.user;
-            console.log(loggedUser);
-            navigate(from, {replace: true});
+           const loggedUser = result.user;
+           console.log(loggedUser);
+           const saveUser = {name: loggedUser.displayName, email: loggedUser.email}
+           fetch('http://localhost:5000/users', {
+               method: 'POST',
+               headers: {
+                   'content-type': 'application/json'
+               },
+               body: JSON.stringify(saveUser)
+              })
+              .then(res => res.json())
+              .then(() => {
+              
+                  
+                   navigate(from, {replace: true});
+               
+              })
+           
+
         })
-    }
+       
+   }
 
     return (
         <div>
